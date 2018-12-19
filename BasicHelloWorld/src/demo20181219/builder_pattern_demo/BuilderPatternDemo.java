91190c78d1a4a7db0404c0bb11d37e1e29cffbe4
yuanyunhao <2286617216@qq.com>
2018-12-19 17:45:45 +0800

    一种循环中使用的语法，可以直接跳出多重循环

--- BasicHelloWorld/src/demo20181219/builder_pattern_demo/BuilderPatternDemo.java
+++ BasicHelloWorld/src/demo20181219/builder_pattern_demo/BuilderPatternDemo.java
@@ -22,9 +22,37 @@ public class BuilderPatternDemo {
         nonVegMeal.showItems();
         System.out.println("Total Cost: " +nonVegMeal.getCost());*/
 
-        getData(1,2,3);
+//        getData(1,2,3);
+
+        scanBreakMethod();
     }
 
+    /**
+     * 测试一种语法，来源为String.toUpperCase
+     * 此语法可以跳出多重循环
+     */
+    static void scanBreakMethod(){
+        System.out.println("START");
+        a: {
+            for (int i = 0; i < 10; i++ ) {
+                System.out.println("START-" + i + "---------------");
+                for (int j = 0; j < 10; j++) {
+                    System.out.println(j);
+                    if (j == 3) {
+//                        break a;
+                        break ;
+                    }
+                }
+                System.out.println("END-" + i + "---------------");
+            }
+        }
+        System.out.println("END");
+    }
+
+    /**
+     * 测试 多参数的时候是否可以循环
+     * @param numbers
+     */
     static void getData(Integer... numbers){
         for (Integer i : numbers){
             System.out.println(i);
