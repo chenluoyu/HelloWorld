abcf3378763d6d2f459913b68438fe8032023700
yuanyunhao <2286617216@qq.com>
2019-01-21 17:56:34 +0800

    部分demo

--- BasicHelloWorld/src/demo2019/month1/day10/Demo01.java
+++ BasicHelloWorld/src/demo2019/month1/day10/Demo01.java
@@ -15,15 +15,20 @@ public class Demo01 {
 //        System.out.println(Integer.valueOf("02"));
 
 
-        String str = "合同变更金额校验失败：2019年1季度、2019年2季度、";
-        System.out.println(str.substring(0,str.length()-1));
+//        String str = "合同变更金额校验失败：2019年1季度、2019年2季度、";
+//        System.out.println(str.substring(0,str.length()-1));
 
 
-        Class<? extends Demo01> aClass = new Demo01().getClass();
+//        Class<? extends Demo01> aClass = new Demo01().getClass();
+//        String classPath = aClass.getResource("").getPath();
+//        System.out.println(classPath);
 
-        String classPath = aClass.getResource("").getPath();
-        System.out.println(classPath);
-
+        double a = 100.00;
+        System.out.println(a/3);
+        System.out.println(a/3.0);
+        int b = 100;
+        System.out.println(b/3);
+        System.out.println(b/3.0);
     }
 
 }
